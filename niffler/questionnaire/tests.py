3e7f01f72119265c8b825f489c88ac78a00a0d76
Ernie1 <870949712@qq.com>
2019-06-27 19:02:50 +0800

    test_ProfileView_get

--- niffler/questionnaire/tests.py
+++ niffler/questionnaire/tests.py
@@ -53,48 +53,12 @@ class TaskTestCase(APITestCase):
         response = self.client.get('/questionnaire/profile/3/')
         self.assertEquals(response.status_code, 404)
 
-
-
-    
-        
-#         """APP用户登录接口成功情况"""
-#         # path使用硬编码，不要使用reverse反解析url，以便在修改url之后能及时发现接口地址变化，并通知接口使用人员
-#         path = '/api/api-token-auth/'
-#         data = {'mobile_phone': '15999999999', 'password': '111111'}
-#         response = self.client.post(path, data)
-#         # response.data是字典对象
-#         # response.content是json字符串对象
-#         self.assertEquals(response.status_code,
-#                           status.HTTP_200_OK,
-#                           '登录接口返回状态码错误: 错误信息: {}'.format(response.content))
-#         self.assertIn('token', response.data, '登录成功后无token返回')
-
-#     def test_app_user_login_with_error_pwd(self):
-#         path = '/api/api-token-auth/'
-#         data = {'mobile_phone': '15999999999', 'password': '123456'}
-#         response = self.client.post(path, data)
-#         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
-#         self.assertJSONEqual('{"errors":["用户名或密码错误。"]}', response.content)
-
-#     def test_get_app_user_profile_success(self):
-#         """成功获取app用户个人信息接口"""
-#         path = '/api/account/user/profile/'
-#         headers = self.get_headers(user=self.user)
-#         response = self.client.get(path, **headers)
-#         # 校验一些关键数据即可
-#         # 如果是创建新数据，不仅要校验返回的状态码和数据，
-#         # 还需要到使用Django ORM去数据库查询数据是否创建成功
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(6, len(response.data))
-#         self.assertIn('url', response.data)
-#         self.assertIn('mobile_phone', response.data)
-#         self.assertIn('avatar', response.data)
-#         self.assertIn('company_name', response.data)
-#         self.assertIn('username', response.data)
-#         self.assertIn('is_inviter', response.data)
-
-#     def test_get_app_user_profile_without_token(self):
-#         """不传token请求获取用户信息接口"""
-#         path = '/api/account/user/profile/'
-#         response = self.client.get(path)
-#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_ProfileView_get(self):
+        response = self.client.get('/questionnaire/profile/')
+        self.assertEquals(response.status_code, 201)
+        self.client.force_authenticate(self.user1)
+        response = self.client.get('/questionnaire/profile/')
+        self.assertEquals(response.status_code, 200)
+        self.client.force_authenticate(None)
+        response = self.client.get('/questionnaire/profile/')
+        self.assertEquals(response.status_code, 201)
